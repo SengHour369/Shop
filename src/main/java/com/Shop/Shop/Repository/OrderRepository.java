a1a3445bf4e3dc9cc1ed81ba46a9729b5bc24e96
SengHour369 <seanghour097328@gmail.com>
2025-11-11 00:28:52 +0700

    update

--- src/main/java/com/Shop/Shop/Repository/OrderRepository.java
+++ src/main/java/com/Shop/Shop/Repository/OrderRepository.java
@@ -1,7 +1,13 @@
 package com.Shop.Shop.Repository;
 
+import com.Shop.Shop.DTO.OrderResponseDTO;
 import com.Shop.Shop.Model.Entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order,Long> {
+    List<Order> findByCustomer_Id(Long customerId);
 }
--- src/main/java/com/Shop/Shop/Service/OrderServiceTemp.java
+++ src/main/java/com/Shop/Shop/Service/OrderServiceTemp.java
@@ -7,9 +7,8 @@ import com.Shop.Shop.Exception.ExceptionHandlerNotFound;
 import java.util.List;
 
 public interface OrderServiceTemp {
-    OrderResponseDTO  findOrderById(Long id);
-    List<OrderResponseDTO> findOrderByAll();
+    List<OrderResponseDTO> findOrderByAll(Long id);
     OrderResponseDTO CreateOrder(OrderRequestDTO orderRequestDTO) throws ExceptionHandlerNotFound;
-    OrderResponseDTO UpdateOrder(Long id ,OrderRequestDTO orderRequestDTO);
-    OrderResponseDTO DeleteOrder(Long id);
+    OrderResponseDTO UpdateOrder(Long id ,OrderRequestDTO orderRequestDTO) throws ExceptionHandlerNotFound;
+    OrderResponseDTO DeleteOrder(Long id) throws ExceptionHandlerNotFound;
 }
